ca5593c9005750887e17203b361e5cd186dd6eda
I-OMEGA-I <the0mega4@yandex.ru>
2023-02-16 16:57:51 +0400

    return laguages to storybook

--- src/shared/config/storybook/TranslationDecorator/TranslationDecorator.tsx
+++ src/shared/config/storybook/TranslationDecorator/TranslationDecorator.tsx
@@ -1,20 +1,19 @@
 import { Story } from '@storybook/react';
-import 'app/styles/index.scss';
 import { Suspense, useEffect } from 'react';
 import { I18nextProvider } from 'react-i18next';
-import { PageLoader } from 'widgets/PageLoader';
-import i18nForTests from '../../i18n/i18nForStorybook';
+import { Loader } from 'shared/ui/Loader';
+import i18nForStorybook from '../../i18n/i18nForStorybook';
 
 export const TranslationDecorator = (story: () => Story, context: any) => {
     const { globals } = context;
 
     useEffect(() => {
-        i18nForTests.changeLanguage(globals.locale);
+        i18nForStorybook.changeLanguage(globals.locale);
     }, [globals.locale]);
 
     return (
-        <Suspense fallback={<PageLoader />}>
-            <I18nextProvider i18n={i18nForTests}>
+        <Suspense fallback={<Loader />}>
+            <I18nextProvider i18n={i18nForStorybook}>
                 {story()}
             </I18nextProvider>
         </Suspense>
