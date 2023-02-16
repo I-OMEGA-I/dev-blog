e5857f7aeb241fed7329a9fc6ab4a0d8d1809249
I-OMEGA-I <the0mega4@yandex.ru>
2023-02-16 16:28:50 +0400

    fix problem with loki

--- src/shared/config/storybook/TranslationDecorator/TranslationDecorator.tsx
+++ src/shared/config/storybook/TranslationDecorator/TranslationDecorator.tsx
@@ -2,6 +2,7 @@ import { Story } from '@storybook/react';
 import 'app/styles/index.scss';
 import { Suspense, useEffect } from 'react';
 import { I18nextProvider } from 'react-i18next';
+import { PageLoader } from 'widgets/PageLoader';
 import i18nForTests from '../../i18n/i18nForStorybook';
 
 export const TranslationDecorator = (story: () => Story, context: any) => {
@@ -12,7 +13,7 @@ export const TranslationDecorator = (story: () => Story, context: any) => {
     }, [globals.locale]);
 
     return (
-        <Suspense fallback="">
+        <Suspense fallback={<PageLoader />}>
             <I18nextProvider i18n={i18nForTests}>
                 {story()}
             </I18nextProvider>
--- src/shared/lib/renderWithTranslation/renderWithTranslation.tsx
+++ src/shared/lib/renderWithTranslation/renderWithTranslation.tsx
@@ -2,10 +2,11 @@ import { render } from '@testing-library/react';
 import { Suspense } from 'react';
 import { I18nextProvider } from 'react-i18next';
 import i18nForTests from 'shared/config/i18n/i18nForTests';
+import { PageLoader } from 'widgets/PageLoader';
 
 export function renderWithTranslation(Component: React.ReactNode) {
     return render(
-        <Suspense fallback="">
+        <Suspense fallback={<PageLoader />}>
             <I18nextProvider i18n={i18nForTests}>
                 {Component}
             </I18nextProvider>
