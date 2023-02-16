e6203c0e17deb22ca1128821947a30803fd28ba5
I-OMEGA-I <the0mega4@yandex.ru>
2023-02-16 21:38:05 +0400

    fix problem with storybook translate

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -2,10 +2,8 @@ import { addDecorator } from '@storybook/react';
 import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/styleDecorator';
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/themeDecorator';
 import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
-import {
-    TranslationDecorator,
-} from '../../src/shared/config/storybook/TranslationDecorator/TranslationDecorator';
 import { Theme } from '../../src/app/providers/ThemeProvider';
+import i18n from './i18next';
 
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
@@ -15,24 +13,14 @@ export const parameters = {
             date: /Date$/,
         },
     },
-};
-
-export const globalTypes = {
-    locale: {
-        name: 'Locale',
-        description: 'Internationalization locale',
-        toolbar: {
-            icon: 'globe',
-            items: [
-                { value: 'en', title: 'English' },
-                { value: 'ka', title: 'ქართული' },
-            ],
-            title: 'locale',
-        },
+    i18n,
+    locale: 'en',
+    locales: {
+        en: { title: 'English', left: '🇺🇸' },
+        ka: { title: 'ქართული', left: '🇬🇪' },
     },
 };
 
 addDecorator(StyleDecorator);
 addDecorator(ThemeDecorator(Theme.LIGHT));
 addDecorator(RouterDecorator);
-addDecorator(TranslationDecorator);
