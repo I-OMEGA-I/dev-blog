72c5de8406931295826a42b0e3ad1db30f678df1
I-OMEGA-I <the0mega4@yandex.ru>
2023-02-23 17:42:08 +0400

    Counter Entity

---
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -0,0 +1,23 @@
+import { Provider } from 'react-redux';
+import { StateSchema } from '../config/StateSchema';
+import { createReduxStore } from '../config/store';
+
+interface StoreProviderProps {
+ children?: React.ReactNode;
+ initialState?: StateSchema;
+}
+
+export const StoreProvider = (props: StoreProviderProps) => {
+    const {
+        children,
+        initialState,
+    } = props;
+
+    const store = createReduxStore(initialState);
+
+    return (
+        <Provider store={store}>
+            {children}
+        </Provider>
+    );
+};
