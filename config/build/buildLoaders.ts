24a7c8941a1894d61d31a16c82d2f0c3b2716f19
I-OMEGA-I <the0mega4@yandex.ru>
2023-02-24 05:42:02 +0400

    some changes

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,4 +1,3 @@
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { RuleSetRule } from 'webpack';
 import buildCssLoader from './loaders/buildCssLoader';
 import { BuildOptions } from './types/config';
